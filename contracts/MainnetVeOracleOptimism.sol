5bb6812ed63efd1177d640cbebca7469a3f7688c
dudesahn <dudesahn@pm.me>
2023-10-22 20:37:45 -0400

    chore: more wip

---
+++ contracts/MainnetVeOracleOptimism.sol
@@ -0,0 +1,150 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity 0.8.19;
+
+import {Ownable2Step} from "@openzeppelin/contracts@4.9.3/access/Ownable2Step.sol";
+
+interface IVeCrv {
+    struct LockedBalance {
+        int128 amount;
+        uint256 end;
+    }
+
+    struct Point {
+        int128 bias;
+        int128 slope;
+        uint256 ts;
+        uint256 blk;
+    }
+
+    function epoch() external view returns (uint256);
+
+    function point_history(uint256) external view returns (Point memory);
+
+    function slope_changes(uint256) external view returns (int128);
+
+    function locked(address _user) external view returns (LockedBalance memory);
+
+    function user_point_epoch(address _user) external view returns (uint256);
+
+    function user_point_history(address _user, uint256 _epoch)
+        external
+        view
+        returns (Point memory);
+}
+
+interface IOracle is IVeCrv {
+    function submit_state(
+        uint256 _epoch,
+        Point memory _globalPointStruct,
+        int128[8] memory _slopeChangeArray,
+        address _user,
+        LockedBalance memory _userLockedStruct,
+        uint256 _userEpoch,
+        Point memory _userPointStruct
+    ) external;
+}
+
+interface IOptimismMessenger {
+    function sendMessage(
+        address _target,
+        bytes memory _message,
+        uint32 _gasLimit
+    ) external;
+}
+
+/**
+ * @title Mainnet veOracle Optimism
+ * @notice This contract reads data from Curve's veCRV contract for a given user, and pushes that data to Optimism to be
+ *  read and used for determining boost for Curve gauges on Optimism.
+ */
+contract MainnetVeOracleOptimism is Ownable2Step {
+    /// @notice Ethereum veCRV contract; pull all of our info from here
+    IVeCrv public constant veCRV =
+        IVeCrv(0x5f3b5DfEb7B28CDbD7FAba78963EE202a494e2A2);
+
+    /// @notice The address of Optimism's messenger contract on L1
+    IOptimismMessenger public constant ovmL1CrossDomainMessenger =
+        IOptimismMessenger(0x25ace71c97B33Cc4729CF772ae268934F7ab5fA1);
+
+    /// @notice The address of our veOracle on Optimism
+    address public optimismVeOracle;
+
+    /// Week in seconds
+    uint256 constant WEEK = 1 weeks;
+
+    /**
+     * @notice Update the Optimism veOracle for a given user.
+     * @param _user The user whose data to push to Optimism's veOracle.
+     */
+    function updateOptimismVeOracle(address _user) external {
+        if (optimismVeOracle == address(0)) {
+            revert("Set optimismVeOracle address first");
+        }
+
+        // here we should pull all of the relevant params we need from veCRV to send to the L2
+        (
+            IVeCrv.LockedBalance memory userLockedStruct,
+            IVeCrv.Point memory userPointStruct,
+            uint256 userEpoch
+        ) = _getUpdatedUserInfo(_user);
+
+        // pull our global point history struct
+        uint256 currentEpoch = veCRV.epoch();
+        IVeCrv.Point memory globalPointStruct = veCRV.point_history(
+            currentEpoch
+        );
+
+        // generate slope changes
+        int128[8] memory slopeChanges;
+        uint256 startTime = (globalPointStruct.ts / WEEK) * WEEK + WEEK;
+        for (uint256 i = 0; i < 8; i++) {
+            slopeChanges[i] = veCRV.slope_changes(startTime + WEEK * i);
+        }
+
+        ovmL1CrossDomainMessenger.sendMessage(
+            optimismVeOracle,
+            abi.encodeWithSignature(
+                "submit_state(uint256,(int128,int128,uint256,uint256),int128[8],address,(int128,uint256),uint256,(int128,int128,uint256,uint256))",
+                currentEpoch,
+                globalPointStruct,
+                slopeChanges,
+                _user,
+                userLockedStruct,
+                userEpoch,
+                userPointStruct
+            ),
+            1000000
+        );
+    }
+
+    /**
+     * @notice Pull info from the veCRV contract for a given user address.
+     * @param _user The user whose data to push to Optimism's veOracle.
+     * @return userLockedStruct Struct containing the user's lock info.
+     * @return userPointStruct Struct containing the user's point info (bias, slope, etc.).
+     * @return userEpoch Current epoch for a given user.
+     */
+    function _getUpdatedUserInfo(address _user)
+        internal
+        view
+        returns (
+            IVeCrv.LockedBalance memory userLockedStruct,
+            IVeCrv.Point memory userPointStruct,
+            uint256 userEpoch
+        )
+    {
+        // get our lock and point struct, as well as our latest user epoch, from veCRV
+        userLockedStruct = veCRV.locked(_user);
+        userEpoch = veCRV.user_point_epoch(_user);
+        userPointStruct = veCRV.user_point_history(_user, userEpoch);
+    }
+
+    /**
+     * @notice Update the Optimism veOracle contract address.
+     * @dev May only be called by owner.
+     * @param _oracleAddress The address for our new veOracle on Optimism.
+     */
+    function setOptimismVeOracle(address _oracleAddress) external onlyOwner {
+        optimismVeOracle = _oracleAddress;
+    }
+}
--- contracts/OptimismVeOracle.sol
+++ contracts/OptimismVeOracle.sol
@@ -1,9 +1,18 @@
-// SPDX-License-Identifier: AGPL-3.0
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
 import {Ownable2Step} from "@openzeppelin/contracts@4.9.3/access/Ownable2Step.sol";
 
-interface IVeCrv {
+interface IOptimismMessenger {
+    function xDomainMessageSender() external returns (address);
+}
+
+/**
+ * @title Optimism veOracle
+ * @notice This contract reports veCRV boost data received from Ethereum. Data is transmitted over Optimism's native
+ *  bridge. Only the specified mainnetVeOracle may update point and lock data for a user.
+ */
+contract OptimismVeOracle is Ownable2Step {
     struct LockedBalance {
         int128 amount;
         uint256 end;
@@ -16,83 +25,142 @@ interface IVeCrv {
         uint256 blk;
     }
 
-    function epoch() external view returns (uint256);
+    // our mainnet oracle address
+    address public mainnetVeOracle;
 
-    function point_history(uint256) external view returns (Point memory);
+    IOptimismMessenger public constant ovmL2CrossDomainMessenger =
+        IOptimismMessenger(0x4200000000000000000000000000000000000007);
 
-    function locked(address _user) external view returns (LockedBalance memory);
+    /// Week in seconds
+    uint256 constant WEEK = 1 weeks;
 
-    function user_point_epoch(address _user) external view returns (uint256);
+    /// Migrated `VotingEscrow` storage variables
+    uint256 public epoch;
+    Point[100000000000000000000000000000] public point_history;
+    mapping(address => uint256) public user_point_epoch;
+    mapping(address => Point[1000000000]) public user_point_history;
 
-    function user_point_history(address _user, uint256 _epoch)
-        external
-        view
-        returns (Point memory);
-}
+    mapping(uint256 => int128) public slope_changes;
+    mapping(address => LockedBalance) public locked;
 
-interface IOptimismMessenger {
-    function sendMessage(
-        address _target,
-        bytes memory _message,
-        uint32 _gasLimit
-    ) external;
-}
+    /// Log a state submission
+    event SubmittedState(address _user, uint256 _userEpoch);
 
-contract OptimismVeOracle is Ownable2Step {
-    IVeCrv public constant veCRV =
-        IVeCrv(0x5f3b5DfEb7B28CDbD7FAba78963EE202a494e2A2);
-
-    IOptimismMessenger public constant ovmL1CrossDomainMessenger =
-        IOptimismMessenger(0x25ace71c97B33Cc4729CF772ae268934F7ab5fA1);
-
-    address public optimismVeOracle;
-
-    function updateOptimismVeOracle(address _user) external {
-        // here we should pull all of the relevant params we need from veCRV to send to the L2
-        (
-            IVeCrv.LockedBalance memory userLockedStruct,
-            IVeCrv.Point memory userPointStruct,
-            uint256 userEpoch
-        ) = _getUpdatedUserInfo(_user);
-
-        // pull our global point history struct
-        uint256 currentEpoch = veCRV.epoch();
-        IVeCrv.Point memory globalPointStruct = veCRV.point_history(
-            currentEpoch
-        );
-
-        ovmL1CrossDomainMessenger.sendMessage(
-            optimismVeOracle,
-            abi.encodeWithSignature(
-                "submit_state(uint256, IVeCrv.Point, IVeCrv.LockedBalance, uint256, IVeCrv.Point)",
-                currentEpoch,
-                globalPointStruct,
-                userLockedStruct,
-                userEpoch,
-                userPointStruct
-            ),
-            1000000
-        );
+    constructor(address _mainnetVeOracle) {
+        mainnetVeOracle = _mainnetVeOracle;
     }
 
-    function _getUpdatedUserInfo(address _user)
-        internal
+    modifier onlyMainnetVeOracle() {
+        require(
+            msg.sender == address(ovmL2CrossDomainMessenger) &&
+                ovmL2CrossDomainMessenger.xDomainMessageSender() ==
+                mainnetVeOracle
+        );
+        _;
+    }
+
+    function balanceOf(address _user) external view returns (uint256) {
+        return balanceOf(_user, block.timestamp);
+    }
+
+    function balanceOf(address _user, uint256 _timestamp)
+        public
         view
-        returns (
-            IVeCrv.LockedBalance memory userLockedStruct,
-            IVeCrv.Point memory userPointStruct,
-            uint256 userEpoch
-        )
+        returns (uint256)
     {
-        // get our lock and point struct info from veCRV
-        lockedStruct = veCRV.locked(_user);
-
-        // get our most recent epoch
-        userEpoch = veCRV.user_point_epoch(_user);
-        userPointStruct = veCRV.user_point_history(_user, userEpoch);
+        uint256 _epoch = user_point_epoch[_user];
+        if (_epoch == 0) {
+            return 0;
+        }
+        Point memory last_point = user_point_history[_user][_epoch];
+        last_point.bias -=
+            last_point.slope *
+            abi.decode(abi.encode(_timestamp - last_point.ts), (int128));
+        if (last_point.bias < 0) {
+            return 0;
+        }
+        return abi.decode(abi.encode(last_point.bias), (uint256));
     }
 
-    function setOptimismVeOracle(address _oracleAddress) onlyOwner {
-        optimismVeOracle = _oracleAddress;
+    function totalSupply() external view returns (uint256) {
+        return totalSupply(block.timestamp);
+    }
+
+    function totalSupply(uint256 _timestamp) public view returns (uint256) {
+        Point memory last_point = point_history[epoch];
+        uint256 t_i = (last_point.ts / WEEK) * WEEK; // value in the past
+        for (uint256 i = 0; i < 255; i++) {
+            t_i += WEEK; // + week
+            int128 d_slope = 0;
+            if (t_i > _timestamp) {
+                t_i = _timestamp;
+            } else {
+                d_slope = slope_changes[t_i];
+                if (d_slope == 0) {
+                    break;
+                }
+            }
+            last_point.bias -=
+                last_point.slope *
+                abi.decode(abi.encode(t_i - last_point.ts), (int128));
+            if (t_i == _timestamp) {
+                break;
+            }
+            last_point.slope += d_slope;
+            last_point.ts = t_i;
+        }
+
+        if (last_point.bias < 0) {
+            return 0;
+        }
+        return abi.decode(abi.encode(last_point.bias), (uint256));
+    }
+
+    // make sure to add back the onlyMainnetVeOracle once done with this testing
+    function submit_state(
+        uint256 _epoch,
+        Point memory _globalPointStruct,
+        int128[8] memory _slopeChangeArray,
+        address _user,
+        LockedBalance memory _userLockedStruct,
+        uint256 _userEpoch,
+        Point memory _userPointStruct
+    ) external {
+        // always set the point_history structs
+        point_history[_epoch] = _globalPointStruct;
+        user_point_history[_user][_userEpoch] = _userPointStruct;
+
+        // increment the epoch storage var only if fresh
+        // also update slope changes too (next 8 values)
+        if (_epoch > epoch) {
+            epoch = _epoch;
+
+            uint256 startTime = (_globalPointStruct.ts / WEEK) * WEEK + WEEK;
+
+            for (uint256 i = 0; i < 8; i++) {
+                slope_changes[startTime + WEEK * i] = abi.decode(
+                    abi.encode(_slopeChangeArray[i]),
+                    (int128)
+                );
+            }
+        }
+
+        // update the user point epoch and locked balance if it is newer
+        if (_userEpoch > user_point_epoch[_user]) {
+            user_point_epoch[_user] = _userEpoch;
+
+            locked[_user] = _userLockedStruct;
+        }
+
+        emit SubmittedState(_user, _userEpoch);
+    }
+
+    /**
+     * @notice Update the Ethereum veOracle contract address.
+     * @dev May only be called by owner.
+     * @param _oracleAddress The address for our veOracle on Ethereum.
+     */
+    function setMainnetVeOracle(address _oracleAddress) external onlyOwner {
+        mainnetVeOracle = _oracleAddress;
     }
 }
